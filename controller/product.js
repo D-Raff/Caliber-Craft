8c6f72d44d815ece085be046f128b3f5428256b7
daanyaalkellerman <kellermandaanyaal2@gmail.com>
2024-02-27 09:49:20 +0200

    error handling for prods

--- controller/product.js
+++ controller/product.js
@@ -2,35 +2,69 @@ import { getProducts,getProduct,addProduct,editProduct,deleteProduct } from "../
 
 export default {
     getProducts: async (req,res)=>{
-        res.send(await getProducts())
+        try{
+            res.send(await getProducts())
+        }catch(e){
+            res.status(404).json({
+                status: res.send(
+                  'Cannot retrive Products'
+                )
+            })
+        }
     },
     getProduct: async (req,res)=>{
-        res.send(await getProduct(+req.params.prodID))
+        try{
+            res.send(await getProduct(+req.params.prodID))
+        }catch(e){
+            res.status(404).json({
+                status: res.send(
+                    'Sorry cannot get that item'
+                )
+            })
+        }
     },
     addProduct: async (req,res)=>{
-        const {prodName,quantity,amount,category,prodUrl,prodBio,prodSingle1,prodSingle2,prodSingle3} = req.body
-        await addProduct(prodName,quantity,amount,category,prodUrl,prodBio,prodSingle1,prodSingle2,prodSingle3)
-        res.send({
-            msg:'New Product Added'
-        })
+        try{
+            const {prodName,quantity,amount,category,prodUrl,prodBio,prodSingle1,prodSingle2,prodSingle3} = req.body
+            await addProduct(prodName,quantity,amount,category,prodUrl,prodBio,prodSingle1,prodSingle2,prodSingle3)
+            res.send({
+                msg:'New Product Added'
+            })
+        }catch(e){
+            res.status(404).json({
+               status: res.send('Cannot add a product')
+            })
+        }
     },
     editProduct:async (req,res)=>{
-        const [product] = await getProduct(+req.params.prodID)
-        let {prodName,quantity,amount,category,prodUrl,prodBio,prodSingle1,prodSingle2,prodSingle3} = req.body
-        prodName ? prodName=prodName: {prodName} = product
-        quantity ? quantity=quantity: {quantity} = product
-        amount ? amount=amount: {amount} = product
-        category ? category=category: {category} = product
-        prodUrl ? prodUrl=prodUrl: {prodUrl} = product
-        prodBio ? prodBio=prodBio: {prodBio} = product
-        prodSingle1 ? prodSingle1=prodSingle1: {prodSingle1} = product
-        prodSingle2 ? prodSingle2=prodSingle2: {prodSingle2} = product
-        prodSingle3 ? prodSingle3=prodSingle3: {prodSingle3} = product
-        await editProduct(+req.params.prodID,prodName,quantity,amount,category,prodUrl,prodBio,prodSingle1,prodSingle2,prodSingle3)
-        res.json(await getProducts())
+        try{
+            const [product] = await getProduct(+req.params.prodID)
+            let {prodName,quantity,amount,category,prodUrl,prodBio,prodSingle1,prodSingle2,prodSingle3} = req.body
+            prodName ? prodName=prodName: {prodName} = product
+            quantity ? quantity=quantity: {quantity} = product
+            amount ? amount=amount: {amount} = product
+            category ? category=category: {category} = product
+            prodUrl ? prodUrl=prodUrl: {prodUrl} = product
+            prodBio ? prodBio=prodBio: {prodBio} = product
+            prodSingle1 ? prodSingle1=prodSingle1: {prodSingle1} = product
+            prodSingle2 ? prodSingle2=prodSingle2: {prodSingle2} = product
+            prodSingle3 ? prodSingle3=prodSingle3: {prodSingle3} = product
+            await editProduct(+req.params.prodID,prodName,quantity,amount,category,prodUrl,prodBio,prodSingle1,prodSingle2,prodSingle3)
+            res.json(await getProducts())
+        }catch(e){
+            res.status(404).json({
+                status: res.send('Unable to edit product')
+            })
+        }
     },
     deleteProduct:async (req,res)=>{
-        res.send(await deleteProduct(req.params.prodID))
+        try{
+            res.send(await deleteProduct(req.params.prodID))
+        }catch(e){
+            res.status(404).json({
+                status:res.send('No product was deleted')
+            })
+        }
     }
     
 }
