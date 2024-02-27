9ac3bc10803fad645e176577571c579694c65b94
D-Raff <damonraffels@gmail.com>
2024-02-27 15:08:20 +0200

    cards completed

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,11 +1,14 @@
 <template>
-    <div class="Products">
+    <div class="container-fluid" id="Products">
         <div class="row" v-if="products">
             <card v-for="product in products" :key="product.prodID">
-                <template #prod-title>
-                    {{ product.prodName }}
+                <template #prod-img>
+                    <img class="prod-img" :src="product.prodUrl">
                 </template>
-                <template #cardBody>
+                <template #prod-title>
+                        {{ product.prodName }}
+                </template>
+                <template #Desc>
                     <p>
                         Category: {{ product.category }}
                     </p>
@@ -15,7 +18,7 @@
                     <p>
                         Quantity: {{ product.quantity }}
                     </p>
-                    <router-link :to="{name: 'product', params: {id: product.prodID}}">View More</router-link>
+                    <router-link :to="{ name: 'product', params: { id: product.prodID } }" id="see-more">View More</router-link>
                 </template>
             </card>
         </div>
@@ -31,8 +34,8 @@ export default {
     components: {
         card
     },
-    computed:{
-        products(){
+    computed: {
+        products() {
             return this.$store.state.products
         }
     },
@@ -41,10 +44,42 @@ export default {
     }
 }
 </script>
-<style>
-    .Products{
-        min-height: 100vh;
-        width: 100%;
-        background: black;
-    }
+<style scoped>
+#Products {
+    min-height: 100vh;
+    width: 100%;
+    background: black;
+}
+
+.prod-img {
+    width: 150px;
+}
+
+.card-headers {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.5s ease-in-out;
+    transform: translate3d(0px, 0px, 50px);
+}
+.card-headers img:hover {
+    transform: translate3d(0px, 0px, 60px);
+}
+
+#see-more {
+    cursor: pointer;
+    display: inline-block;
+    font-weight: 900;
+    font-size: 9px;
+    text-transform: uppercase;
+    color: rgb(7, 185, 255);
+    padding: 0.5rem 0.7rem;
+    transition: all 0.5s ease-in-out;
+    transform: translate3d(0px, 0px, 20px);
+}
+#see-more:hover {
+  transform: translate3d(0px, 0px, 60px);
+}
+
 </style>
