6a80acb2e4dca66895d222a36e1483da7460b61c
daanyaalkellerman <kellermandaanyaal2@gmail.com>
2024-02-29 12:23:10 +0200

    fix

--- controller/product.js
+++ controller/product.js
@@ -39,18 +39,19 @@ export default {
     },
     editProduct:async (req,res)=>{
         try{
-            const [product] = await getProduct(+req.params.prodID)
+            const product = await getProduct(+req.params.prodID)
             let {prodName,quantity,amount,category,prodUrl,prodBio,prodDes} = req.body
-            prodName ? prodName=prodName: {prodName} = product
-            quantity ? quantity=quantity: {quantity} = product
-            amount ? amount=amount: {amount} = product
-            category ? category=category: {category} = product
-            prodUrl ? prodUrl=prodUrl: {prodUrl} = product
-            prodBio ? prodBio=prodBio: {prodBio} = product
-            prodDes ? prodDes=prodDes: {prodDes} = product
+            prodName ? prodName=prodName: {prodName}=product
+            quantity ? quantity=quantity: {quantity}=product
+            amount ? amount=amount: {amount}=product
+            category ? category=category: {category}=product
+            prodUrl ? prodUrl=prodUrl: {prodUrl}=product
+            prodBio ? prodBio=prodBio: {prodBio}=product
+            prodDes ? prodDes=prodDes: {prodDes}=product
             await editProduct(+req.params.prodID,prodName,quantity,amount,category,prodUrl,prodBio,prodDes)
             res.json(await getProducts())
         }catch(e){
+            console.log(e)
             res.status(404).json({
                 status:404,
                 msg:'Unable to edit product'
