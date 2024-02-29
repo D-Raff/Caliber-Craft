f2f4fa0da55f15deb4f7742dc3ce6c4b72cb286d
daanyaalkellerman <kellermandaanyaal2@gmail.com>
2024-02-29 11:30:26 +0200

    access control added

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ config()
 const PORT = process.env.MYSQL_ADDON_PORT
 const app = express()
 
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    next();
+})
 app.use(cors()) 
 app.use(express.json())
 app.use(cookieParser())
